f655aae4c291f65f688cdb889537ec0d40805166
matt <matt1235@naver.com>
2023-07-25 00:14:10 +0900

    Feat: Initial Commit 폴더 구조 및 의존성, 설정 파일 생성

---
+++ plot-server/src/main/java/com/plot/plotserver/PlotServerApplication.java
@@ -0,0 +1,13 @@
+package com.plot.plotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlotServerApplication.class, args);
+	}
+	
+}
---
+++ plot-server/src/test/java/com/plot/plotserver/PlotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plot.plotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
