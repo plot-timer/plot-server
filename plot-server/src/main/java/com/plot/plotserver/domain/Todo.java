9bec9aeaa3555f37d89ab9f4fcbd4d88e10c6e46
matt <matt1235@naver.com>
2023-07-25 11:15:23 +0900

    Feat: History, Todo 엔티티 생성 #1

---
+++ plot-server/src/main/java/com/plot/plotserver/domain/Todo.java
@@ -0,0 +1,63 @@
+package com.plot.plotserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, columnDefinition = "varchar (36)")
+    private String title;
+
+    @Column(name = "subtitle", nullable = true, columnDefinition = "varchar (36)")
+    private String subTitle;
+
+    @Column(name = "memo", nullable = true, columnDefinition = "varchar (36)")
+    private String memo;
+
+    @Column(name = "seq_index", nullable = false, columnDefinition = "int")
+    private int seqIndex;
+
+    @Column(name = "total_time", nullable = false, columnDefinition = "time")
+    private Time totalTime;
+
+    @Column(name = "start_date", nullable = false, columnDefinition = "datetime")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = true, columnDefinition = "datetime")
+    private LocalDateTime endDate;
+
+    @Column(name = "cur_time", nullable = false, columnDefinition = "time")
+    private Time curTime;
+
+    @Column(name = "star", nullable = false, columnDefinition = "bit (1)")
+    private boolean star;
+
+    @Column(name = "icon_image_path", nullable = true, columnDefinition = "text")
+    private String iconImagePath;
+
+    @Column(name = "user_id", nullable = false, columnDefinition = "bigint")
+    private Long userId;
+
+    @Column(name = "category_id", nullable = false, columnDefinition = "bigint")
+    private Long categoryId;
+
+    @Column(name = "category_name", nullable = false, columnDefinition = "varchar (36)")
+    private Long categoryName;
+
+}
