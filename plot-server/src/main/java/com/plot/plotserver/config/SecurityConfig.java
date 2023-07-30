368e17d22921908d23d8f4f8a87009e3bb94fde9
matt <matt1235@naver.com>
2023-07-31 01:55:20 +0900

    Feat: SecurityConfig 클래스 구현 #5 시큐리티 설정 클래스 구현: SecurityConfig

---
+++ plot-server/src/main/java/com/plot/plotserver/config/SecurityConfig.java
@@ -0,0 +1,107 @@
+package com.plot.plotserver.config;
+
+import com.plot.plotserver.repository.RefreshTokenRepository;
+import com.plot.plotserver.repository.UserRepository;
+import com.plot.plotserver.security.auth.CustomUserDetailsService;
+import com.plot.plotserver.security.auth.JwtAuthenticationFilter;
+import com.plot.plotserver.security.auth.JwtAuthenticationProvider;
+import com.plot.plotserver.security.auth.JwtAuthorizationFilter;
+import com.plot.plotserver.security.handler.CustomLogoutHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CustomUserDetailsService customUserDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+        JwtAuthenticationFilter jwtAuthenticationFilter = jwtAuthenticationFilter(authenticationManager);
+        JwtAuthorizationFilter jwtAuthorizationFilter = jwtAuthorizationFilter(authenticationManager);
+
+        http
+                .cors();
+
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);    // jwt사용으로 세션관리 해제
+
+        http
+                .headers().frameOptions().sameOrigin();
+
+        http
+                .logout()
+                .logoutUrl("/api/logout")
+                .logoutSuccessHandler(new CustomLogoutHandler())
+                .deleteCookies("plot_token");
+
+        http
+                .authorizeRequests()
+                .antMatchers("/api/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/test/**")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/api/social-login/**", "/api/login", "/api/user/one")
+                .permitAll()
+                .antMatchers("/api/**")
+                .access("hasRole('ROLE_USER')")
+                .anyRequest().permitAll();
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAfter(jwtAuthorizationFilter, JwtAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationProvider jwtAuthenticationProvider() throws Exception {
+        return new JwtAuthenticationProvider(customUserDetailsService, passwordEncoder());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        JwtAuthenticationFilter authenticationFilter = new JwtAuthenticationFilter(refreshTokenRepository);
+        authenticationFilter.setAuthenticationManager(authenticationManager);
+
+        SecurityContextRepository contextRepository = new HttpSessionSecurityContextRepository();
+        authenticationFilter.setSecurityContextRepository(contextRepository);
+
+        return authenticationFilter;
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter(AuthenticationManager authenticationManager) {
+        JwtAuthorizationFilter authorizationFilter = new JwtAuthorizationFilter(authenticationManager, userRepository, refreshTokenRepository);
+        return authorizationFilter;
+    }
+}
