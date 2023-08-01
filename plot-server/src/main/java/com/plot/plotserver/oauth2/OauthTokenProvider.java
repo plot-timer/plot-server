6e3b5156692d2b415a0e93e8f6af07f0c740bad9
matt <matt1235@naver.com>
2023-08-01 11:28:35 +0900

    Feat: OAuth2 Provider 클래스 구현(토큰, 유저 정보) #11

---
+++ plot-server/src/main/java/com/plot/plotserver/oauth2/OauthTokenProvider.java
@@ -0,0 +1,73 @@
+package com.plot.plotserver.oauth2;
+
+import com.plot.plotserver.dto.request.login.SocialLoginReqDto;
+import com.plot.plotserver.dto.response.login.OauthTokenResponseDto;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+public class OauthTokenProvider {
+
+    public static OauthTokenResponseDto getTokenFromAuthServer(SocialLoginReqDto req, ClientRegistration provider) throws Exception {
+        String providerName = req.getProvider();
+        String code = req.getCode();
+
+        if (providerName.equals("naver")) {
+            return getNaverToken(code, provider);
+        } else if (providerName.equals("kakao")) {
+            return getKakaoToken(code, provider);
+        } else {
+            throw new Exception("허용되지 않습니다.");
+        }
+    }
+
+    public static OauthTokenResponseDto getKakaoToken(String code, ClientRegistration provider) {
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+
+        formData.add("grant_type", "authorization_code");
+        formData.add("client_id", provider.getClientId());
+        formData.add("client_secret", provider.getClientSecret());
+        formData.add("redirect_uri", provider.getRedirectUri());
+        formData.add("code", code);
+
+        return WebClient.create()
+                .post()
+                .uri(provider.getProviderDetails().getTokenUri())
+                .headers(header -> {
+                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
+                })
+                .bodyValue(formData)
+                .retrieve()
+                .bodyToMono(OauthTokenResponseDto.class)
+                .block();
+    }
+
+    public static OauthTokenResponseDto getNaverToken(String code, ClientRegistration provider) {
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "authorization_code");
+        formData.add("client_id", provider.getClientId());
+        formData.add("client_secret", provider.getClientSecret());
+        formData.add("code", code);
+        formData.add("state", "1234");
+
+        return WebClient.create()
+                .post()
+                .uri(provider.getProviderDetails().getTokenUri())
+                .headers(header -> {
+                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
+                })
+                .bodyValue(formData)
+                .retrieve()
+                .bodyToMono(OauthTokenResponseDto.class)
+                .block();
+    }
+}
