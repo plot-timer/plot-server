3e5ce3a7ed3cdc994860c0fe2b0fcf282c763938
matt <matt1235@naver.com>
2023-08-04 00:37:55 +0900

    Feat: 테스트 로직 추가 #14 테스트 로직 추가 - 테스트 끝난 후 반드시 지워야 함 1. 유저가 로그인 된 상태에서 자신의 정보를 확인할 수 있음 2. 현재 회원가입 된 유저 모두의 정보 출력

--- plot-server/src/main/java/com/plot/plotserver/controller/UserController.java
+++ plot-server/src/main/java/com/plot/plotserver/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.plot.plotserver.controller;
 
+import com.plot.plotserver.domain.Message;
 import com.plot.plotserver.dto.request.user.UserReqDto;
 import com.plot.plotserver.dto.response.user.UserResponseDto;
 import com.plot.plotserver.service.UserService;
+import com.plot.plotserver.util.SecurityContextHolderUtil;
 import lombok.RequiredArgsConstructor;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,7 +48,46 @@ public class UserController {
     )
     @DeleteMapping("/one")
     public ResponseEntity<Boolean> deleteOne(@RequestBody UserReqDto.DeleteUser req) throws Exception {
-        return ResponseEntity.ok(userService.deleteOne(req));
+        return ResponseEntity.ok(userService.deleteOne());
+    }
+
+
+
+    /**
+     * TEST URL : 테스트가 끝나면 필수적으로 지워야 함.
+     * */
+    @Operation(summary = "로그인 테스트 api", description = "로그인 상태에서, 해당 유저의 정보를 출력.")
+    @GetMapping("/test1")
+    public ResponseEntity<?> TestApi() throws Exception {
+        Long userId = SecurityContextHolderUtil.getUserId();
+        try {
+            UserResponseDto userResDto = UserResponseDto.of(userService.findOne(userId));
+            Message message = Message.builder()
+                    .data(userResDto)
+                    .status(HttpStatus.OK)
+                    .message("success")
+                    .build();
+            return new ResponseEntity<>(message, message.getStatus());
+        }catch (Exception e) {
+            Message message = Message.builder()
+                    .status(HttpStatus.BAD_REQUEST)
+                    .message("searching_error")
+                    .message(e.getMessage())
+                    .build();
+            return new ResponseEntity<>(message, message.getStatus());
+        }
+    }
+
+    @GetMapping("/test2")
+    public ResponseEntity<?> searchAll(){
+        List<UserResponseDto> result = userService.findAll();
+
+        Message message = Message.builder()
+                .data(result)
+                .status(HttpStatus.OK)
+                .message("success")
+                .build();
+        return new ResponseEntity<>(message, message.getStatus());
     }
 
 }
