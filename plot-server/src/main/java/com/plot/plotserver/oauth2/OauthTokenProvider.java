e0c1fa79b358ca9a31c9644d3e9b6794bbdd1669
Hoo-sung <gntjd135@gmail.com>
2023-08-16 14:09:43 +0900

    Feat: 구글 소셜 로그인 기능 구현  #31
          -OAuth2UserInfo interface 수정, 추후 소셜 로그인 계정 연동 고려
          -Username에 이메일 저장하도록 수정.

--- plot-server/src/main/java/com/plot/plotserver/oauth2/OauthTokenProvider.java
+++ plot-server/src/main/java/com/plot/plotserver/oauth2/OauthTokenProvider.java
@@ -2,15 +2,20 @@ package com.plot.plotserver.oauth2;
 
 import com.plot.plotserver.dto.request.login.SocialLoginReqDto;
 import com.plot.plotserver.dto.response.login.OauthTokenResponseDto;
-import org.springframework.http.MediaType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.Map;
 
+
+@Slf4j
 public class OauthTokenProvider {
 
     public static OauthTokenResponseDto getTokenFromAuthServer(SocialLoginReqDto req, ClientRegistration provider) throws Exception {
@@ -21,7 +26,10 @@ public class OauthTokenProvider {
             return getNaverToken(code, provider);
         } else if (providerName.equals("kakao")) {
             return getKakaoToken(code, provider);
-        } else {
+        } else if(providerName.equals("google")){
+            return getGoogleToken(code,provider);
+        }
+        else {
             throw new Exception("허용되지 않습니다.");
         }
     }
@@ -70,4 +78,34 @@ public class OauthTokenProvider {
                 .bodyToMono(OauthTokenResponseDto.class)
                 .block();
     }
+
+    public static OauthTokenResponseDto getGoogleToken(String code, ClientRegistration provider) {
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "authorization_code");
+        formData.add("client_id", provider.getClientId());
+        formData.add("client_secret", provider.getClientSecret());
+        formData.add("code", code);
+        formData.add("redirect_uri", provider.getRedirectUri());
+
+        log.info("client_id={}", provider.getClientId());
+        log.info("client_secret={}", provider.getClientSecret());
+        log.info("code={}",code);
+        log.info("redirect_uri={}", provider.getRedirectUri());
+
+        return WebClient.create()
+                .post()
+                .uri(provider.getProviderDetails().getTokenUri())
+                .headers(header -> {
+                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
+                })
+                .bodyValue(formData)
+                .retrieve()
+                .bodyToMono(OauthTokenResponseDto.class)
+                .block();
+
+
+    }
+
 }
