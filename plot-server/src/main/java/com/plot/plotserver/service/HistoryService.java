d0bf9e5b9179d22d20c26ee5bc1663d3ab2d2059
matt <matt1235@naver.com>
2023-08-25 21:57:07 +0900

    Fix : History 생성 오류 수정 #43
    - 히스토리 생성 시, 날짜가 첫날이 저장되지 않는 오류 수정
    - 히스토리 생성 시 해당 날짜에 같은 투두의, daily-todo가 존재하는지 확인하는 로직 추가 => 해당 날짜에 같은 투두의 daily-todo가 존재하지 않는 경우에만 새로 생성해준다,

--- plot-server/src/main/java/com/plot/plotserver/service/HistoryService.java
+++ plot-server/src/main/java/com/plot/plotserver/service/HistoryService.java
@@ -2,6 +2,7 @@ package com.plot.plotserver.service;
 
 import com.plot.plotserver.domain.DailyTodo;
 import com.plot.plotserver.domain.Record;
+import com.plot.plotserver.domain.Todo;
 import com.plot.plotserver.dto.request.history.HistoryReqDto;
 import com.plot.plotserver.exception.history.HistorySavedFailException;
 import com.plot.plotserver.repository.DailyTodoRepository;
@@ -16,6 +17,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,47 +32,51 @@ public class HistoryService {
 
         try {
 
+            DailyTodo foundDailyTodo = dailyTodoRepository.findById(dailyTodoId).get();
+            Todo todo = foundDailyTodo.getTodo();
+
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
             LocalDateTime startDateTime = LocalDateTime.parse(historyReqDto.getStartDate(), formatter); // 시작 날짜 및 시간을 LocalDateTime으로 변환
             LocalDateTime endDateTime = LocalDateTime.parse(historyReqDto.getEndDate(), formatter); // 종료 날짜 및 시간을 LocalDateTime으로 변환
 
             LocalDate startDate = startDateTime.toLocalDate();
-            LocalDate endDate = endDateTime.toLocalDate();
-
             LocalTime startTime = startDateTime.toLocalTime();
+
+            LocalDate endDate = endDateTime.toLocalDate();
             LocalTime endTime = endDateTime.toLocalTime();
 
-
-            DailyTodo firstDayDailyTodo = dailyTodoRepository.findById(dailyTodoId).get();
 
             while (!startDate.isAfter(endDate)) {
                 LocalDateTime startDateWithTime = startDate.atTime(startTime);
                 LocalDateTime endDateWithTime;
 
-                if (startDate.isEqual(endDate)) {
+                if (startDate.isEqual(endDate)) {   // 시작 날짜 == 끝 날짜
                     endDateWithTime = endDate.atTime(endTime);
-                } else {
-                    endDateWithTime = startDate.atTime(LocalTime.MAX);//23:59:59로 설정.
+                } else {    // 시작 날짜 < 끝 날짜
+                    endDateWithTime = startDate.atTime(LocalTime.of(23,59,59));//23:59:59로 설정.
                 }
 
-                Duration duration = Duration.between(startDateWithTime, endDateWithTime);
-                long totalSeconds = duration.getSeconds();
+                // 해당 날짜에 이미 같은 투두의, daily-todo가 존재하는지 확인
+                Optional<DailyTodo> dailyTodoOpt = dailyTodoRepository.findByTodoIdAndDailyTodoDate(todo.getId(), startDate);
 
                 DailyTodo dailyTodoToSave;
-                if (startDate.equals(startDateTime.toLocalDate())) {//처음 날이면, 받은 dailyTodoId로 저장.
-                    dailyTodoToSave = firstDayDailyTodo;
-                }
 
-                else{//새로운 dailyTodo만들고, 애와 연결된 history 저장.
+                if(!dailyTodoOpt.isPresent()){  // 존재하지 않는다면, 새로 생성해준다.
                     DailyTodo dailyTodo = DailyTodo.builder()
                             .dailyTodoDate(startDate)
                             .status(DailyTodoStatusEnum.EMPTY)
-                            .todo(firstDayDailyTodo.getTodo())
-                            .user(firstDayDailyTodo.getUser())
+                            .todo(todo)
+                            .user(foundDailyTodo.getUser())
                             .build();
 
                     dailyTodoToSave = dailyTodoRepository.save(dailyTodo);
                 }
+                else    // 이미 해당 날짜에 데일리 투두가 존재
+                    dailyTodoToSave = dailyTodoOpt.get();
+
+                Duration duration = Duration.between(startDateWithTime, endDateWithTime);
+                long totalSeconds = duration.getSeconds();
+
                 Record record = Record.builder()
                         .isHistory(true)
                         .startDate(startDateWithTime)
