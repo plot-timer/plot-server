4e58b08abfa82a2c3caa68e6acbfc3f20d97369c
matt <matt1235@naver.com>
2023-09-10 17:20:39 +0900

    Feat & Fix : 스케줄 생성과 수정하는 로직 통합 #65

--- plot-server/src/main/java/com/plot/plotserver/repository/RecordRepository.java
+++ plot-server/src/main/java/com/plot/plotserver/repository/RecordRepository.java
@@ -2,17 +2,19 @@ package com.plot.plotserver.repository;
 
 import com.plot.plotserver.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
-
 
 
     @Query("SELECT r FROM Record r WHERE r.dailyTodo.id = :dailyTodoId")
@@ -26,5 +28,8 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 
     @Query("SELECT r FROM Record r WHERE r.isHistory = false AND r.dailyTodo.id = :dailyTodoId")
     List<Record> findSchedulesByDailyTodoId(Long dailyTodoId);
+
+    @Query("SELECT r FROM Record r WHERE r.isHistory = false AND r.scheduledDate = :date AND r.dailyTodo.user.id = :userId")
+    List<Record> findSchedulesByDateAndUser(Long userId, LocalDate date);
 
 }
